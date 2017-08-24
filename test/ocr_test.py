d82d4b31af2cd76695b2905054d2585e91d860b4
remilepriol <remi.lp.17@gmail.com>
2017-08-24 01:05:39 -0400

    Add Marginals.entropy and divergence test

--- test/ocr_test.py
+++ test/ocr_test.py
@@ -5,14 +5,26 @@ import ocr
 ########################################################################################################################
 # MARGINALS CLASS
 ########################################################################################################################
-marg = ocr.Marginals(word_length=5)
+word = ocr.word2list("beta")
+word_length = len(word)
+marg = ocr.Marginals(word_length=word_length)
 print("With uniform values.")
 print("densities ?", marg.is_densities())
 print("consistent?", marg.is_consistent())
 print()
 print("With random values.")
-marg.unary = np.random.rand(5, ocr.ALPHABET_SIZE)
+marg.unary = np.random.rand(word_length, ocr.ALPHABET_SIZE)
 print("densities ?", marg.is_densities())
 print("consistent?", marg.is_consistent())
-marg.binary = np.random.rand(4, ocr.ALPHABET_SIZE, ocr.ALPHABET_SIZE)
-print("consistent?", marg.is_consistent())
+marg.binary = np.random.rand(word_length - 1, ocr.ALPHABET_SIZE, ocr.ALPHABET_SIZE)
+print("consistent?", marg.is_consistent(), "\n")
+
+marg = ocr.Marginals(word_length=word_length)
+u2 = np.zeros([word_length, ocr.ALPHABET_SIZE])
+b2 = np.zeros([word_length - 1, ocr.ALPHABET_SIZE, ocr.ALPHABET_SIZE])
+u2[np.arange(word_length), word] = 1
+b2[np.arange(word_length - 1), word[:-1], word[1:]] = 1
+marg2 = ocr.Marginals(unary=u2, binary=b2)
+print("Uniform entropy :", marg.entropy(), "\t T*log(26) :", word_length * np.log(ocr.ALPHABET_SIZE))
+print("Dirac Entropy :", marg2.entropy())
+print("Divergence :", marg2.kullback_leibler(marg))
