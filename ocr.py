ad7d4c9f4bcce434dff3a223248b3b65ccd6ee53
remilepriol <remi.lp.17@gmail.com>
2017-09-11 20:55:45 -0400

    Transfer some functions from jupyter
    Namely sdca, Features.display(), Marginals.display, radius, radii, and
    initialization methods.

--- ocr.py
+++ ocr.py
@@ -1,9 +1,11 @@
 # standard imports
 import csv
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+import random_counters
 # custom imports
 import utils
 
@@ -256,14 +258,14 @@ def unary_scores(images, weights):
     return uscores
 
 
-def binary_scores(images, weights):
+def binary_scores(word_length, weights):
     """Return the binary scores of word given by weights.
 
-    :param images:
+    :param word_length:
     :param weights:
     :return:
     """
-    bscores = np.empty([images.shape[0] - 1, ALPHABET_SIZE, ALPHABET_SIZE])
+    bscores = np.empty([word_length - 1, ALPHABET_SIZE, ALPHABET_SIZE])
     bscores[:] = np.reshape(weights[select_transition(-1, -1)], (ALPHABET_SIZE, ALPHABET_SIZE))
     # the code below is more understandable but slower
     # for t in range(word.shape[0]-1):
@@ -310,7 +312,7 @@ def backward_forward(unary_scores, binary_scores):
         binary_marginals[t] = np.exp(forward_messages[t - 1, :, np.newaxis] + unary_scores[t, :, np.newaxis]
                                      + binary_scores[t] + unary_scores[t + 1] + backward_messages[t + 1])
 
-    return unary_marginals, binary_marginals
+    return unary_marginals, binary_marginals, log_partition
 
 
 ########################################################################################################################
@@ -350,7 +352,7 @@ class Features:
             self.unary[:, -3] = images.shape[0] / ALPHABET_SIZE
             self.unary[:, -2:] = 1 / ALPHABET_SIZE
         else:
-            # tmp is a vertical concatenation the images in the word and the bias terms
+            # tmp is a vertical concatenation of the images in the word and the bias terms
             tmp = np.empty([images.shape[0], NB_PIXELS + 3])
             tmp[:, :-3] = images
             tmp[:, -3] = 1
@@ -388,8 +390,25 @@ class Features:
         binary = feat[select_transition(-1, -1)].reshape(ALPHABET_SIZE, -1)
         return Features(unary, binary)
 
+    def display(self):
+        emissions = letters2wordimage(self.unary[:, :-3])
+        plt.matshow(emissions)
+        ticks_positions = np.linspace(0, emissions.shape[1], ALPHABET_SIZE + 2).astype(int)[1:-1]
+        plt.xticks(ticks_positions, list(ALPHABET))
+        plt.colorbar(fraction=0.046, pad=0.04)
+        plt.matshow(self.binary)
+        plt.xticks(range(26), [ALPHABET[x] for x in range(26)])
+        plt.yticks(range(26), [ALPHABET[x] for x in range(26)])
+        plt.colorbar(fraction=0.046, pad=0.04)
+        plt.title("Transition Features")
+        rescale_bias = np.array([1 / 7.5, 1, 1])
+        plt.matshow((self.unary[:, -3:] * rescale_bias).T)
+        plt.xticks(range(26), [ALPHABET[x] for x in range(26)])
+        plt.colorbar(fraction=0.046, pad=0.04)
+        plt.title("Bias features")
 
-class Marginals():
+
+class Marginals:
     def __init__(self, unary=None, binary=None, word_length=None):
         if unary is None or binary is None:
             self.length = word_length
@@ -433,21 +452,26 @@ class Marginals():
         return utils.kullback_leibler(self.binary, other_marginals.binary) \
                - utils.kullback_leibler(self.unary[1:-1], other_marginals.unary[1:-1])
 
+    def inner_product(self, other_marginals):
+        """Return the special inner product where the marginals on the separations are subtracted."""
+        return np.sum(self.binary * other_marginals.binary) - \
+               np.sum(self.unary[1:-1] * other_marginals.unary[1:-1])
+
     def add(self, other_marginals):
         return Marginals(unary=self.unary + other_marginals.unary,
-                         binary=self.binary + other_marginals.unary)
+                         binary=self.binary + other_marginals.binary)
 
     def subtract(self, other_marginals):
         return Marginals(unary=self.unary - other_marginals.unary,
-                         binary=self.binary - other_marginals.unary)
+                         binary=self.binary - other_marginals.binary)
 
     def multiply(self, other_marginals):
         return Marginals(unary=self.unary * other_marginals.unary,
-                         binary=self.binary * other_marginals.unary)
+                         binary=self.binary * other_marginals.binary)
 
     def divide(self, other_marginals):
         return Marginals(unary=self.unary / other_marginals.unary,
-                         binary=self.binary / other_marginals.unary)
+                         binary=self.binary / other_marginals.binary)
 
     def multiply_scalar(self, scalar):
         return Marginals(unary=scalar * self.unary,
@@ -457,23 +481,14 @@ class Marginals():
         return Marginals(unary=self.unary ** 2, binary=self.binary ** 2)
 
     def log(self):
-        return Marginals(unary=np.log(np.maximum(1e-16, self.unary)),
-                         binary=np.log(np.maximum(1e-16, self.binary)))
-
-    def inner_product(self, other_marginals):
-        """ Return the special inner product where the marginals on the separations are subtracted.
-
-        :param other_marginals:
-        :return:
-        """
-        return np.sum(self.binary, other_marginals.binary) - \
-               np.sum(self.unary[1:-1] * other_marginals.unary[1:-1])
+        return Marginals(unary=np.log(np.maximum(1e-50, self.unary)),
+                         binary=np.log(np.maximum(1e-50, self.binary)))
 
     @staticmethod
     def infer_from_weights(images, weights):
         uscores = unary_scores(images, weights)
-        bscores = binary_scores(images, weights)
-        umargs, bmargs = backward_forward(uscores, bscores)
+        bscores = binary_scores(images.shape[0], weights)
+        umargs, bmargs, _ = backward_forward(uscores, bscores)
         return Marginals(unary=umargs, binary=bmargs)
 
     @staticmethod
@@ -484,3 +499,263 @@ class Marginals():
         bmargs = np.zeros([word_length - 1, ALPHABET_SIZE, ALPHABET_SIZE])
         bmargs[np.arange(word_length - 1), labels[:-1], labels[1:]] = 1
         return Marginals(unary=umargs, binary=bmargs)
+
+    def display(self):
+        plt.matshow(self.unary)
+        plt.xticks(range(26), [ALPHABET[x] for x in range(26)])
+        plt.colorbar(fraction=0.046, pad=0.04)
+        plt.title("unary marginals")
+        plt.matshow(self.binary.sum(axis=0))
+        plt.xticks(range(26), [ALPHABET[x] for x in range(26)])
+        plt.yticks(range(26), [ALPHABET[x] for x in range(26)])
+        plt.colorbar(fraction=0.046, pad=0.04)
+        plt.title("sum of binary marginals")
+
+
+########################################################################################################################
+# NEW STUFF
+########################################################################################################################
+
+def radius(word):
+    # The factor 2 comes from the difference : ground truth - other label
+    r = 2 * np.sum(word ** 2)  # emission
+    r += 2 * word.shape[0]  # model bias
+    r += 2 * 2  # beginning and end of word biases
+    r += 2 * (word.shape[0] - 1)  # transitions
+    return r
+
+
+def radii(words):
+    nb_words = words.shape[0]
+    rs = np.empty(nb_words)
+    for i in range(nb_words):
+        rs[i] = radius(words[i])
+    return rs
+
+
+# initialize with uniform marginals
+def uniform_marginals(labels):
+    nb_words = labels.shape[0]
+    margs = np.empty(nb_words, dtype=np.object)
+    for i in range(nb_words):
+        margs[i] = Marginals(word_length=labels[i].shape[0])
+    return margs
+
+
+# Or marginals from the ground truth
+def gt_marginals(labels):
+    nb_words = labels.shape[0]
+    margs = np.empty(nb_words, dtype=np.object)
+    for i, lbls in enumerate(labels):
+        margs[i] = Marginals.dirac(lbls)
+    return margs
+
+
+# Initialize the weights as the centroid of the ground truth features minus the centroid of the
+# features given by the uniform marginals.
+def uniform_weights(images, labels):
+    nb_words = labels.shape[0]
+    ground_truth_centroid = Features()
+    uniform_centroid = Features()
+    for label, image in zip(labels, images):
+        word_size = label.shape[0]
+        if word_size != image.shape[0]:
+            raise ValueError("Not the same number of labels (%i) and images (%i) inside word." \
+                             % (word_size, image.shape[0]))
+        ground_truth_centroid.add_word(label, image)
+        uniform_centroid.add_centroid(image)
+    ground_truth_centroid = ground_truth_centroid.to_array() / nb_words
+    uniform_centroid = uniform_centroid.to_array() / nb_words
+    return ground_truth_centroid - uniform_centroid
+
+
+def dual_score(weights, marginals, regularization_parameter):
+    ans = - regularization_parameter / 2 * np.sum(weights ** 2)
+    ans += sum([margs.entropy() for margs in marginals]) / marginals.shape[0]
+    return ans
+
+
+def duality_gaps(marginals, weights, images):
+    ans = []
+    for margs, imgs in zip(marginals, images):
+        newmargs = Marginals.infer_from_weights(imgs, weights)
+        ans.append(margs.kullback_leibler(newmargs))
+    return np.array(ans)
+
+
+def sdca(x, y, regularization_parameter, npass=5, update_period=5, precision=1e-5, subprecision=1e-16, non_uniformity=0,
+         step_size=False, _debug=False):
+    """Update alpha and w with the stochastic dual coordinate ascent algorithm to fit the model to the
+    data points x and the labels y.
+
+    :param x: data points organized by rows
+    :param y: labels as a one dimensional array. They should be positive.
+    :param npass: maximum number of pass over the data
+    :param precision: precision to which we wish to optimize the objective.
+    :param non_uniformity: between 0 and 1. probability of sampling non-uniformly.
+    :param _debug: if true, return a detailed list of objectives
+    :return: the list of duality gaps after each pass over the data
+    :return: the time after each pass over the data
+    """
+
+    ##################################################################################
+    # INITIALIZE : the dual and primal variables
+    ##################################################################################
+    nb_words = y.shape[0]
+    regu = - regularization_parameter * nb_words / 2
+    if nb_words != x.shape[0]:
+        raise ValueError("Not the same number of labels (%i) and images (%i) inside training set." \
+                         % (nb_words, x.shape[0]))
+    marginals = uniform_marginals(y)
+    weights = uniform_weights(x, y) / regularization_parameter
+    # marginals = gt_marginals(y)
+    # w = np.zeros(ocr.NB_FEATURES)
+
+    ##################################################################################
+    # OBJECTIVES
+    ##################################################################################
+    entropies = np.array([margs.entropy() for margs in marginals])
+    dual_objective = entropies.mean() - regularization_parameter / 2 * np.sum(weights ** 2)
+    delta_time = time.time()
+    timing = [0]
+
+    ##################################################################################
+    # NON-UNIFORM SAMPLING : initialize the sampler
+    ##################################################################################
+    if non_uniformity > 0:
+        dgaps = duality_gaps(marginals, weights, regularization_parameter, x)
+        sampler = random_counters.RandomCounters(dgaps)
+        duality_gap = dgaps.mean()
+        obj = [duality_gap]
+    else:
+        obj = [dual_objective]
+        duality_gap = 1
+
+    ##################################################################################
+    # COUNTERS : to give insights on the algorithm
+    ##################################################################################
+    annex = []
+    countneg = 0
+    countpos = 0
+    countzero = 0
+
+    ##################################################################################
+    # MAIN LOOP
+    ##################################################################################
+    t = 0
+    while t < nb_words * npass and duality_gap > precision:
+        t += 1
+
+        ##################################################################################
+        # DRAW : one sample at random.
+        ##################################################################################
+        if np.random.rand() > non_uniformity:  # then sample uniformly
+            i = np.random.randint(nb_words)
+        else:  # sample proportionally to the duality gaps
+            i = sampler.sample()
+
+        ##################################################################################
+        # MARGINALIZATION ORACLE
+        ##################################################################################
+        margs_i = Marginals.infer_from_weights(x[i], weights)
+
+        ##################################################################################
+        # DUALITY GAP ESTIMATE : for the non-uniform sampling
+        ##################################################################################
+        if non_uniformity > 0:
+            sampler.update(marginals[i].kullback_leibler(margs_i), i)
+
+        ##################################################################################
+        # ASCENT DIRECTION : and primal movement
+        ##################################################################################
+        dual_direction = margs_i.subtract(marginals[i])
+        primal_direction = Features()
+        primal_direction.add_centroid(x[i], dual_direction)
+        # Centroid of the corrected features in the dual direction
+        # = Centroid of the real features in the opposite of the dual direction
+        primal_direction = - primal_direction.to_array() / regularization_parameter / nb_words
+
+        if step_size:
+            gammaopt = step_size
+        else:
+            ##################################################################################
+            # LINE SEARCH : find the optimal alpha[i]
+            ##################################################################################
+            quadratic_coeff = regu * np.sum(primal_direction ** 2)
+            linear_coeff = 2 * regu * np.dot(weights, primal_direction)
+
+            # line search function and its derivatives
+            # def f(gamma):
+            #    newmargs = marginals[i].add(dual_direction.multiply_scalar(gamma))
+            #    return  newmargs.entropy() + gamma**2 * quadratic_coeff + gamma * linear_coeff
+
+            def gf(gamma):
+                newmargs = marginals[i].add(dual_direction.multiply_scalar(gamma))
+                return - dual_direction.inner_product(newmargs.log()) + gamma * 2 * quadratic_coeff + linear_coeff
+
+            def ggf(gamma):
+                newmargs = marginals[i].add(dual_direction.multiply_scalar(gamma))
+                return - dual_direction.inner_product(dual_direction.divide(newmargs)) + 2 * quadratic_coeff
+
+            gammaopt, subobjective = utils.find_root_decreasing(gf, ggf, precision=subprecision)
+            if _debug:
+                annex.append([quadratic_coeff, linear_coeff, gammaopt])
+
+            ##################################################################################
+            # COUNTERS
+            ##################################################################################
+            if subobjective[-1] > subprecision:
+                countpos += 1
+            elif subobjective[-1] < -subprecision:
+                countneg += 1
+            else:
+                countzero += 1
+
+        ##################################################################################
+        # UPDATE : the primal and dual coordinates
+        ##################################################################################
+        marginals[i] = marginals[i].add(dual_direction.multiply_scalar(gammaopt))
+        weights += gammaopt * primal_direction
+
+        if not step_size:
+            tmp = marginals[i].entropy()
+            dual_objective += (
+                              tmp - entropies[i] + gammaopt ** 2 * quadratic_coeff + gammaopt * linear_coeff) / nb_words
+            entropies[i] = tmp
+            annex[-1].append(dual_objective)
+
+        ##################################################################################
+        # OBJECTIVES : after each pass over the data, compute the duality gap
+        ##################################################################################
+        if t % nb_words == 0:
+            if non_uniformity <= 0:
+                t1 = time.time()
+                obj.append(dual_score(weights, marginals, regularization_parameter))
+                t2 = time.time()
+                delta_time += t2 - t1  # Don't count the time spent monitoring the function
+            ###################################################################################
+            ## DUALITY GAPS: perform a batch update after every update_period epochs
+            ## To reduce the staleness for the non-uniform sampling
+            ## To monitor the objective and provide a stopping criterion
+            ##################################################################################
+            # if t % (update_period * n) == 0:
+            else:
+                dgaps = duality_gaps(marginals, weights, regularization_parameter, x)
+                sampler = random_counters.RandomCounters(dgaps)
+                duality_gap = np.mean(dgaps)
+                obj.append(duality_gap)
+
+            timing.append(time.time() - delta_time)
+
+    ##################################################################################
+    # COUNTERS
+    ##################################################################################
+    print("Perfect line search : %i \t Negative ls : %i \t Positive ls : %i" % (countzero, countneg, countpos))
+
+    ##################################################################################
+    # FINISH : convert the objectives to simplify the after process.
+    ##################################################################################
+    obj = np.array(obj)
+    annex = np.array(annex)
+    timing = np.array(timing)
+    return marginals, weights, obj, timing, annex
