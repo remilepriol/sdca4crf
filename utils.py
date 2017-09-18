4e5d8113219645800d0809b483e4d6af6468d2dc
remilepriol <remi.lp.17@gmail.com>
2017-09-18 17:10:24 -0400

    Add methods to handle log(probabilities) in utils

--- utils.py
+++ utils.py
@@ -6,9 +6,17 @@ def entropy(proba, axis=None):
     return -np.sum(proba * np.log(np.maximum(epsilon, proba)), axis=axis)
 
 
+def log_entropy(logproba, axis=None):
+    return -np.sum(np.exp(logproba) * logproba, axis=axis)
+
+
 def kullback_leibler(p, q, axis=None):
     epsilon = 1e-50
     return np.maximum(0, np.sum(p * np.log(np.maximum(p, epsilon) / np.maximum(q, epsilon)), axis=axis))
+
+
+def log_kullback_leibler(logp, logq, axis=None):
+    return np.sum(np.exp(logp) * (logp - logq), axis=axis)
 
 
 def logsumexp(v, axis=-1):
