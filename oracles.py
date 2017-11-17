578b4a9bdfe8666e475fb8c046dbc98377c0f772
remilepriol <remi.lp.17@gmail.com>
2017-11-16 20:06:31 -0500

    Rename the variables in sum-product to be more concise

--- oracles.py
+++ oracles.py
@@ -3,48 +3,50 @@ import numpy as np
 import utils
 
 
-def sequence_sum_product(uscores, bscores):
+def sequence_sum_product(us, bs):
     """Apply the sum-product algorithm on a chain
 
-    :param uscores: array T*K, (unary) scores on individual nodes
-    :param bscores: array (T-1)*K*K, (binary) scores on the edges
+    :param us: array T*K, (unary) scores on individual nodes
+    :param bs: array (T-1)*K*K, (binary) scores on the edges
     :return: log-marginals on nodes, log-marginals on edges, log-partition
     """
 
-    # I keep track of the log messages instead of the messages
+    # I keep track of the islog messages instead of the messages
     # This is more stable numerically
 
-    chain_length, nb_class = uscores.shape
+    length, nb_class = us.shape
+    bm = np.zeros([length - 1, nb_class])  # backward_messages
+    fm = np.zeros([length - 1, nb_class])  # forward_messages
 
     # backward pass
-    backward_messages = np.zeros([chain_length - 1, nb_class])
-    backward_messages[-1] = utils.logsumexp(bscores[-1] + uscores[-1])
-    for t in range(chain_length - 3, -1, -1):
-        backward_messages[t] = utils.logsumexp(bscores[t] + uscores[t + 1] + backward_messages[t + 1])
+    bm[-1] = utils.logsumexp(bs[-1] + us[-1])
+    for t in range(length - 3, -1, -1):
+        bm[t] = utils.logsumexp(bs[t] + us[t + 1] + bm[t + 1])
 
-    # we compute the log-partition and include it in the forward messages
-    log_partition = utils.logsumexp(backward_messages[0] + uscores[0])
+    # we compute the islog-partition and include it in the forward messages
+    log_partition = utils.logsumexp(bm[0] + us[0])
 
     # forward pass
-    forward_messages = np.zeros([chain_length - 1, nb_class])
-    forward_messages[0] = utils.logsumexp(bscores[0].T + uscores[0] - log_partition)
-    for t in range(1, chain_length - 1):
-        forward_messages[t] = utils.logsumexp(bscores[t].T + uscores[t] + forward_messages[t - 1])
+    fm[0] = utils.logsumexp(bs[0].T + us[0] - log_partition)
+    for t in range(1, length - 1):
+        fm[t] = utils.logsumexp(bs[t].T + us[t] + fm[t - 1])
 
-    unary_marginals = np.empty([chain_length, nb_class])
-    unary_marginals[0] = uscores[0] + backward_messages[0] - log_partition
-    unary_marginals[-1] = forward_messages[-1] + uscores[-1]
-    for t in range(1, chain_length - 1):
-        unary_marginals[t] = forward_messages[t - 1] + uscores[t] + backward_messages[t]
+    # unary marginals
+    umargs = np.empty([length, nb_class])
+    umargs[0] = us[0] + bm[0] - log_partition
+    umargs[-1] = fm[-1] + us[-1]
+    for t in range(1, length - 1):
+        umargs[t] = fm[t - 1] + us[t] + bm[t]
 
-    binary_marginals = np.empty([chain_length - 1, nb_class, nb_class])
-    binary_marginals[0] = uscores[0, :, np.newaxis] + bscores[0] + uscores[1] + backward_messages[1] - log_partition
-    binary_marginals[-1] = forward_messages[-2, :, np.newaxis] + uscores[-2, :, np.newaxis] + bscores[-1] + uscores[-1]
-    for t in range(1, chain_length - 2):
-        binary_marginals[t] = forward_messages[t - 1, :, np.newaxis] + uscores[t, :, np.newaxis] + bscores[t] + uscores[
-            t + 1] + backward_messages[t + 1]
+    # binary marginals
+    bmargs = np.empty([length - 1, nb_class, nb_class])
+    bmargs[0] = us[0, :, np.newaxis] + bs[0] + us[1] + bm[1] - log_partition
+    bmargs[-1] = fm[-2, :, np.newaxis] + us[-2, :, np.newaxis] + bs[-1] + us[-1]
+    for t in range(1, length - 2):
+        bmargs[t] = fm[t - 1, :, np.newaxis] + us[t, :, np.newaxis] + bs[t] + us[
+            t + 1] + bm[t + 1]
 
-    return unary_marginals, binary_marginals, log_partition
+    return umargs, bmargs, log_partition
 
 
 def sequence_viterbi(uscores, bscores):
