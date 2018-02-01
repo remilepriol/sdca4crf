49a581e68679cbdda80309601da017aa1621e33c
remilepriol <remi.lp.17@gmail.com>
2018-02-01 16:46:49 -0500

    Remove structured array from sparse centroid

--- sparse_centroid.py
+++ sparse_centroid.py
@@ -5,20 +5,13 @@ class SparseCentroid:
 
     def __init__(self, sentence, marginal):
         alphalen = marginal.nb_class
-        # sentence_length, nb_attributes = sentence.shape
+        sentence_length, nb_attributes = sentence.shape
 
-        sent = np.empty_like(sentence, dtype=[('attribute', 'i4'), ('word position', 'i2')])
-        # first column is the sentence array
-        sent['attribute'] = sentence
-        # second column is the word number
-        sent['word position'] = np.arange(sentence.shape[0])[:, np.newaxis]
-        sent = np.ravel(sent)
-        sent.sort(order='attribute')
-        active_attributes, inverse = np.unique(sent['attribute'], return_inverse=True)
-        active_attributes = active_attributes[1:]  # remove zero
+        sent = np.ravel(sentence)
+        active_attributes, inverse = np.unique(sent, return_inverse=True)
         centroid = np.zeros([active_attributes.shape[0], alphalen])
-        for index, value in zip(inverse, sent):
-            centroid[index] += marginal[sent['word position']]
+        for i, inv in enumerate(inverse):
+            centroid[inv] += marginal[i // nb_attributes]
 
         # Finally remove the zeros
         self.active = active_attributes[1:]
