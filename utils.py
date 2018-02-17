375a25b42b59f65255962b98bfb6a976915bd0ec
remilepriol <remi.lp.17@gmail.com>
2018-02-17 14:40:25 -0500

    Remove utils.logsumexp that's not used anymore

--- utils.py
+++ utils.py
@@ -30,12 +30,6 @@ def kullback_leibler(logp, logq, axis=None, returnlog=False):
             print("too big ", ans)
             raise
 
-
-def logsumexp(v, axis=-1):
-    vmax = np.amax(v, axis=axis, keepdims=True)
-    return vmax.squeeze(axis=axis) + np.log(np.sum(np.exp(v - vmax), axis=axis))
-
-
 def boolean_encoding(y, k):
     """Return the n*k matrix Y whose line i is the one-hot encoding of y_i."""
     n = y.shape[0]
