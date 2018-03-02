80e2b517dc731801ca29d014167ae06df4057368
remilepriol <remi.lp.17@gmail.com>
2018-03-01 23:24:04 -0500

    Purify the sparse_centroid module

--- sdca4crf/sparse_centroid.py
+++ sdca4crf/sparse_centroid.py
@@ -1,49 +1,43 @@
 import numpy as np
 
-from .weights import Weights
+from .weights2 import WeightsWithoutEmission
 
 
-class SparsePrimalDirection(Weights):
-    def __init__(self, bias=None, transition=None, nb_labels=0, nb_features=0):
-        super().__init__(bias=bias, transition=transition, nb_features=nb_features, nb_labels=nb_labels)
+class SparsePrimalDirection(WeightsWithoutEmission):
 
-    def _add_centroid_emission(self, points_sequence, unary_marginals):
-        self.emission = SparseCentroid(sentence=points_sequence,
-                                       marginal=unary_marginals, nb_labels=self.nb_labels)
+    def __init__(self, points_sequence, marginals):
+        super().__init__(nb_labels=marginals.nb_labels)
+        super().add_centroid(points_sequence, marginals)
 
-    def add_to_dense(self):
-        return self.emission
+        self.sparse_emission = SparseEmission(points_sequence, marginals)
 
-    def multiply_scalar(self, scalar, inplace=False):
-        if inplace:
-            self.bias = scalar * self.bias
-            self.transition = scalar * self.transition
-            self.emission.centroid = scalar * self.emission.centroid
-        else:
-            return -1
-
-    def inner_product(self, other):
-        return np.sum(self.emission[:, self.active] * other.emission) + \
-               np.sum(self.bias * other.bias) + \
-               np.sum(self.transition * other.transition)
+    def __imul__(self, scalar):
+        super().__imul__(scalar)
+        self.sparse_emission *= scalar
 
     def squared_norm(self):
-        import ipdb; ipdb.set_trace()
-        return np.sum(self.emission.centroid ** 2) + np.sum(self.bias ** 2) + np.sum(self.transition ** 2)
+        ans = super().squared_norm()
+        return ans + self.sparse_emission.squared_norm()
 
 
-class SparseCentroid:
+class SparseEmission:
 
-    def __init__(self, sentence, marginal, nb_labels):
-        alphalen = nb_labels  # marginal.nb_class
-        sentence_length, nb_attributes = sentence.shape
+    def __init__(self, points_sequence, marginals):
+        alphalen = marginals.nb_labels
+        sentence_length, nb_attributes = points_sequence.shape
 
-        active_attributes, inverse = np.unique(sentence, return_inverse=True)
+        active_attributes, inverse = np.unique(points_sequence, return_inverse=True)
         centroid = np.zeros([active_attributes.shape[0], alphalen])
-        # TODO reshpae inverse
+        # TODO reshape inverse
         for i, inv in enumerate(inverse):
-            centroid[inv] += marginal[i // nb_attributes]
+            centroid[inv] += marginals[i // nb_attributes]
 
         # Finally remove the zeros
-        self.active = active_attributes[1:]
-        self.centroid = centroid[1:]
+        self.active_set = active_attributes[1:]
+        self.values = centroid[1:]
+
+    def __imul__(self, scalar):
+        self.values *= scalar
+
+    def squared_norm(self):
+        return np.sum(self.values ** 2)
