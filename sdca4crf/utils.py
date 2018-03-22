634c66beddcde2778013e39c4ecb2b775fb55a8f
remilepriol <remi.lp.17@gmail.com>
2018-03-22 16:15:55 -0400

    Remove try catch to let jit work

--- sdca4crf/utils.py
+++ sdca4crf/utils.py
@@ -2,28 +2,22 @@ import numpy as np
 from numba import jit
 
 
-@jit
+@jit(cache=True)
 def entropy(logproba, axis=None, returnlog=False):
     themax = np.amax(logproba)
-    try:
-        ans = themax + np.log(- np.sum(np.exp(logproba - themax) * logproba, axis=axis))
-        if returnlog:
-            return ans
-        else:
-            return np.exp(ans)
-    except FloatingPointError:
-        print("Entropy problem:",
-              themax, "\n",
-              logproba)
-        raise
+    ans = themax + np.log(- np.sum(np.exp(logproba - themax) * logproba, axis=axis))
+    if returnlog:
+        return ans
+    else:
+        return np.exp(ans)
 
 
-@jit
+@jit(cache=True)
 def kullback_leibler(logp, logq, axis=None, returnlog=False):
     themax = np.amax(logp)
     tmp = np.sum(np.exp(logp - themax) * (logp - logq), axis=axis)
     if tmp <= -1e-8:
-        raise Warning(f"Numerical stability: {tmp}")
+        raise Warning("Numerical stability issue: {} should be positive.".format(tmp))
     if tmp <= 0:
         if returnlog:
             return -np.infty
@@ -34,11 +28,7 @@ def kullback_leibler(logp, logq, axis=None, returnlog=False):
     if returnlog:
         return ans
     else:
-        try:
-            return np.exp(ans)
-        except FloatingPointError:
-            print("too big ", ans)
-            raise
+        return np.exp(ans)
 
 
 def letters2wordimage(letters_images):
